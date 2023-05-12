95b618d1761f8e78cb0212345399762d3f2b7433
Sheba <gensheba@gmail.com>
2023-05-12 15:55:27 +0300

    updated the index file

--- src/index.js
+++ src/index.js
@@ -24,28 +24,47 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 function showWeather(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#current_temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let cityTempElement = document.querySelector("#city-temp");
+  let cityNameElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityNameElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+
+//   document.querySelector("#city-name").innerHTML = response.data.name;
+//   document.querySelector("#current_temp").innerHTML = Math.round(
+//     response.data.main.temp
+//   );
+
+//   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+//   document.querySelector("#wind").innerHTML = Math.round(
+//     response.data.wind.speed
+//   );
+//   document.querySelector("#description").innerHTML =
+//     response.data.weather[0].main;
 }
-function searchCity(city) {
+function search(city) {
   let apiKey = "027f18bf6718adb3a32024a0c5a5a173";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(searchCity);
+  axios.get(apiUrl).then(showWeather);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
 function searchLocation(position) {
